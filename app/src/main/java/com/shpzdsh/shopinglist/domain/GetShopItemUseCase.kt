c4be1e2bf3d4b151539067bc0058fbbfbd8a4989
Valentin Sidorov <168553669+Shpzdsh@users.noreply.github.com>
2025-05-17 17:13:38 +0400

    add data.shopListRepositoryImpl

--- app/src/main/java/com/shpzdsh/shopinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/shpzdsh/shopinglist/domain/GetShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.shpzdsh.shopinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(shooItemId: Int): ShopItem {
-        return shopListRepository.getShopItem(shooItemId)
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
     }
 }
